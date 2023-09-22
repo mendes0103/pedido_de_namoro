452137dec759d4a8f62a543cfb3d0eecbe36624e
mendes0103 <113317561+mendes0103@users.noreply.github.com>
2023-09-22 10:12:13 -0300

    Add files via upload

---
+++ README.md/script.js
@@ -0,0 +1,7 @@
+function redirecionarParaConvite() {
+  window.location.href = "convite";  // Redireciona para a página de convite
+}
+
+function respostaErrada() {
+  window.location.href = "covite nao";  // Redireciona para a página de resposta errada
+}
